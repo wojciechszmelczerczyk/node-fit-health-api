c51eb75205a976863aa0087665409c2286a3edb4
Wojciech <szmelczerczykwojciech@gmail.com>
2023-01-22 21:39:10 +0100

    using clarifai grpc, project idea changes

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# ChatGPT API
+# Food app
 
 ## Description
 
-ChatGPT REST API for plants care.
+Food app.[![food-classifier-model](https://clarifai.com/wszmelczerczyk/food-recognition-model/models/food-classifier-model/badge)](https://clarifai.com/wszmelczerczyk/food-recognition-model/models/food-classifier-model?inputId=https%3A%2F%2Fstaticsmaker.iplsc.com%2Fsmaker_prod_2016_08_24%2Fc2f328e43dacecd17cf86ad214c781d2-lg.jpg)
 
 ## Table of contents
 
--- index.js
+++ index.js
@@ -2,16 +2,67 @@ const express = require("express");
 const multer = require("multer");
 const upload = multer();
 
+const { ClarifaiStub, grpc } = require("clarifai-nodejs-grpc");
+
+const stub = ClarifaiStub.grpc();
+
+const metadata = new grpc.Metadata();
+metadata.set("authorization", `Key ${process.env.API_KEY}`);
+
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.post("/upload", upload.single("plant"), (req, res) => {
-  const image = req.file.buffer.toString("base64");
+const predictImage = function (inputs) {
+  return new Promise((resolve, reject) => {
+    stub.PostModelOutputs(
+      {
+        // This is the model ID of a publicly available General model. You may use any other public or custom model ID.
+        model_id: "aaa03c23b3724a16a56b629203edc62c",
+        inputs,
+      },
+      metadata,
+      (err, response) => {
+        if (err) {
+          reject("Error: " + err);
+          return;
+        }
+        console.log(response.status.code);
+        if (response.status.code !== 10000) {
+          console.log(response.status.description);
+          reject(
+            "Received failed status: " +
+              response.status.description +
+              "\n" +
+              response.status.details
+          );
+          return;
+        }
+        let results = [];
+        console.log("Predicted concepts, with confidence values:");
+        for (const c of response.outputs[0].data.concepts) {
+          console.log(c.name + ": " + c.value);
+          results.push({ name: c.name, value: c.value });
+        }
+        resolve(results);
+      }
+    );
+  });
+};
 
-  res.send("work");
+app.post("/", async (req, res) => {
+  try {
+    const { imageUrl } = req.body;
+
+    const inputs = [{ data: { image: { url: imageUrl } } }];
+
+    const results = await predictImage(inputs);
+    return res.json(results);
+  } catch (err) {
+    return res.status(400).send({ error: err.message });
+  }
 });
 
 app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
