b757cac4311b9a59b99c885459584e7199eb8640
Wojciech <szmelczerczykwojciech@gmail.com>
2023-01-25 01:37:46 +0100

    readme modified

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-# Food app
+# Fit health API
 
 [![food-item-recognition](https://clarifai.com/clarifai/main/models/food-item-recognition/badge)](https://clarifai.com/clarifai/main/models/food-item-recognition)
 
 ## Description
 
-Food app.
+API for Fitness app.
 
 ## Table of contents
 
--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const { config } = require("dotenv");
 const express = require("express");
+var cors = require("cors");
 const {
   predictImageController,
 } = require("./controllers/predictImageController");
 config();
 
 const app = express();
+
+app.use(cors());
 
 const PORT = process.env.PORT || 3000;
 
